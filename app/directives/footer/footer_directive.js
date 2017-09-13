b4ee8b0e699d25a6cf3cc7969d84ca1383612367
Anish Choudhry <choudharyanish2011@gmail.com>
2017-09-14 04:45:36 +0530

    footer

---
+++ app/directives/footer/footer_directive.js
@@ -0,0 +1,31 @@
+
+(function() {
+    
+    'use strict';
+
+    angular.module('directives')
+        .directive('footerDirective', footerDirective);
+
+    footerDirective.$inject = [];
+    function footerDirective() {
+        return {
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'directives/footer/footer_directive.html',
+            link: function(scope) {
+
+                /* ======================================== Var ==================================================== */
+                /* ======================================== Services =============================================== */
+             
+                /* ======================================== Public Methods ========================================= */
+                
+                /* ======================================== Private Methods ======================================== */
+              
+                function init() {
+                    
+                }
+                init();
+            }
+        };
+    }
+})();
